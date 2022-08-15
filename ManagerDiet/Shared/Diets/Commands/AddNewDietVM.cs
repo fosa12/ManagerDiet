791a4a9698fa0abf36bedd7d4cba40ba3b3943fc
fosa12 <licznar99@gmail.com>
2022-08-15 08:59:57 +0200

    add new pages

---
+++ ManagerDiet/Shared/Diets/Commands/AddNewDietVM.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagerDiet.Shared.Diets.Commands
+{
+    public class AddNewDietVM
+    {
+        public string DietName { get; set; }
+        public string DietShortDescryption { get; set; }
+        public string GlycemicIndex { get; set; }
+        public string QuantityProtein { get; set; }
+        public string QuantityCarbo { get; set; }
+        public string QuantityFat { get; set; }
+
+    }
+}
+
